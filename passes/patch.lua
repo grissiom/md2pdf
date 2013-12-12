816789efc12201121566dc33d5696fc9e91444b6
Grissiom <chaos.proton@gmail.com>
2013-12-12 11:18:18 +0800

    patch the markdown to add figure \label

--- passes/patch.lua
+++ passes/patch.lua
@@ -9,3 +9,25 @@ Pass{
 	return input
     end
 }
+
+Pass{
+    name = 'add latex figure label',
+    phase = 'md',
+    part = 'body',
+    proc = function (input)
+        st = {}
+        for line in string.gmatch(input, '[^\r\n]*') do
+            if #line == 0 then
+                table.insert(st, '\n')
+            else
+                name, other = string.match(line, '!%[(.*)%](.*)')
+                if name then
+                    table.insert(st, '!['..name..'\\label{fg:'..name..'}]'..other)
+                else
+                    table.insert(st, line)
+                end
+            end
+        end
+        return table.concat(st, '')
+    end
+}
